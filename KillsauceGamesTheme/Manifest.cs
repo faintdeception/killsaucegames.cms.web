e5f14c4dc892a0f8ffb4a94104a6ddb4d86c73dc
Zach Ruffin <faintdeception@gmail.com>
2020-02-08 09:14:23 -0500

    Initial commit!

---
+++ KillsauceGamesTheme/Manifest.cs
@@ -0,0 +1,9 @@
+using OrchardCore.DisplayManagement.Manifest;
+
+[assembly: Theme(
+    Name = "Killsauce Games Theme",
+    Author = "The Orchard Team",
+    Website = "https://orchardproject.net",
+    Version = "2.0.6",
+    Description = "A theme adapted from the agency theme."
+)]
